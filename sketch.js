0323b4e37fa6cbcc34c6ef743dad494dc9e126a1
Himanshu255-art <73885430+Himanshu255-art@users.noreply.github.com>
2021-01-06 15:32:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+//Create variables here
+var dog,happyDog,database,foodS,foodStock;
+var dogImg;
+function preload()
+{
+  //load images here
+  dogHappy=loadImage("happydog.png")
+  dogImg=loadImage("a.png")
+}
+
+function setup() {
+	createCanvas(500, 500);
+  database=firebase.database();
+  dog=createSprite(250,250)
+  dog.addImage(dogImg)
+  dog.scale=0.2
+
+  foodStock=database.ref('Food')
+  foodStock.on("value",readStock)
+}
+
+
+function draw() {  
+background(46,130,87)
+
+  drawSprites();
+  //add styles here
+
+  if(keyWentDown(UP_ARROW)){
+    
+    dog.addImage(dogHappy)
+  }
+
+}
+function readStock(data){
+  foodS=data.val();
+}
+
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+  database.ref('/').update({
+    Food:x
+  })
+}
+
+
+
